886932064b91554c6d63d1d01d74c44f4a028c64
anhtu268 <anhtu26896@github.com>
2019-04-19 17:40:22 +0700

    Implement basic login

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "2.6.1"
 
 gem "rails", "~> 5.2.3"
 gem "bootstrap-sass", "3.3.7"
+gem "jquery-rails"
 gem "bcrypt", "3.1.12"
 gem "sqlite3"
 gem "puma", "~> 3.11"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save {email.downcase!}
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: {maximum: Settings.validates.user.maximum_name}
   validates :email, presence: true, length: {maximum: Settings.validates.user.maximum_email},
@@ -8,4 +9,10 @@ class User < ApplicationRecord
   validates :password, presence: true, length: {minimum: Settings.validates.user.minimum_pass}
 
   has_secure_password
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+  end
 end
