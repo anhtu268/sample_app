c4bd8ffed18d71ed9e891fe2482926d7484c4057
anhtu268 <anhtu26896@github.com>
2019-04-19 17:40:22 +0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find_by id: params[:id]
-    return if @user
-    flash[:danger] = t "controllers.users.show_error"
-    redirect_to users_url
+  before_action :logged_in_user, except: %i(new show)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: %i(destroy)
+  before_action :load_user, except: %i(new index create)
+
+  def index
+    @users = User.order(:name).page(params[:page]).per Settings.controllers.users.pag
   end
+
+  def show; end
 
   def new
     @user = User.new
@@ -15,14 +19,57 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = t "controllers.users.create_success"
-      redirect_to @user
+      redirect_to users_url
     else
       render :new
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "controllers.users.update_success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "controllers.users.destroy_success"
+    else
+      flash[:danger] = t "controllers.users.destroy_fail"
+    end
+    redirect_to users_url
+  end
+
   private
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "controllers.users.login_check"
+    redirect_to login_url
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t "controllers.users.load_user_error"
+    redirect_to users_url
   end
 end
